f0d72ffac5cfe5156261488326d49babd78d39bb
Piotr Mankowski <pmanko@uw.edu>
2023-07-27 22:17:48 +0000

    Update to github actions

--- build-image.sh
+++ build-image.sh
@@ -4,8 +4,11 @@ set -o allexport; source ./.env; set +o allexport
 
 TAG_NAME=${1:-latest}
 TAG_VERSION=${VERSION:-latest}
-
+PUSH=${2:-false}
 
 docker build -t itechuw/hie-botswana:"$TAG_NAME" -t itechuw/hie-botswana:"$TAG_VERSION" .
-docker push itechuw/hie-botswana:"$TAG_NAME"
-docker push itechuw/hie-botswana:"$TAG_VERSION"
+
+if [ "$PUSH" = true ]; then
+  docker push itechuw/hie-botswana:"$TAG_NAME"
+  docker push itechuw/hie-botswana:"$TAG_VERSION"
+fi
