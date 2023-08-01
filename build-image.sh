d228af56bc40aa0ec82cf921f47421984a924f3b
Piotr Mankowski <pmanko@uw.edu>
2023-08-01 21:39:14 +0000

    More CI debugging

--- build-image.sh
+++ build-image.sh
@@ -3,11 +3,11 @@
 
 TAG_NAME=${1:-latest}
 ENV_FILE=${2:-./.env.hie}
-TAG_VERSION=${VERSION:-latest}
-PUSH=${2:-false}
 
 set -o allexport; source "$ENV_FILE"; set +o allexport
 
+TAG_VERSION=${VERSION:-latest}
+PUSH=${2:-false}
 
 docker build -t itechuw/hie-botswana:"$TAG_NAME" -t itechuw/hie-botswana:"$TAG_VERSION" .
 
