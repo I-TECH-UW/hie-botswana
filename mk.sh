7ec787ed3cb2035a8424f02249beec1ab5678bab
Piotr Mankowski <pmanko@uw.edu>
2023-11-15 01:43:58 +0000

    Updates for SHR etc.

--- mk.sh
+++ mk.sh
@@ -3,14 +3,16 @@
 ./build-image.sh
 
 ./instant package remove -n shared-health-record --env-file .env
-./instant package remove -n message-bus-kafka --env-file .env
+# ./instant package remove -n message-bus-kafka --env-file .env
+# ./instant package remove -n monitoring --env-file .env
 #./instant package remove -n client-registry-opencr --env-file .env
 #./instant package remove -n openhim-mediator-fhir-converter --env-file .env
 #./instant package remove -n reverse-proxy-nginx --env-file .env
 #./instant package remove -n interoperability-layer-openhim --env-file .env
 #./instant package remove -n openhim-mediator-omang-service --env-file .env
 
-./instant package init -n message-bus-kafka --env-file .env
+# ./instant package init -n monitoring --env-file .env
+# ./instant package init -n message-bus-kafka --env-file .env
 #./instant package init -n client-registry-opencr --env-file .env
 #./instant package init -n openhim-mediator-fhir-converter --env-file .env
 #./instant package init -n reverse-proxy-nginx --env-file .env
